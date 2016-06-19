40c324e3bd8f70ea074d94711f5b5c806dba3467
Loamhoof <soeren.medard@gmail.com>
2016-06-19 16:47:18 +0200

    Generate sound files on the fly

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 
+import contextlib
+import gtts
 import http.server
+import os
 import soco
+import urllib.parse
 
 
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+SOUND_DIR = os.path.join(CURRENT_DIR, 'sounds')
 SCORES_URI = 'http://192.168.10.155:8000'
 SERVER_ADDRESS = ('', 8888)
-sonos = soco.discover().pop()
-print(sonos)
 
 
 def main():
@@ -16,9 +20,17 @@ def main():
 
 class YTDownloadHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        scores = self.path.split('/')
-        sonos.play_uri('%s/%s_%s.mp3' % (SCORES_URI, scores[1], scores[2]))
+        quoted_text = self.path[1:]
+        text = urllib.parse.unquote(quoted_text)
+        sound_path = os.path.join(SOUND_DIR, '%s.mp3' % text)
+        if os.path.exists(sound_path) is False:
+            gtts.gTTS(text=text, lang='fr').save(sound_path)
+        sonos.play_uri('%s/%s.mp3' % (SCORES_URI, quoted_text))
 
 
 if __name__ == '__main__':
+    sonos = soco.discover().pop()
+    print(sonos)
+    with contextlib.suppress(FileExistsError):
+        os.mkdir(SOUND_DIR)
     main()
