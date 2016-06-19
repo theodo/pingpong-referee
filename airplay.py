5960e5d108c3fb09848d7e353ef028e845aa81c7
Thomas Perrot <thomas.perrot@cybelangel.com>
2016-06-19 15:17:06 +0200

    Added english

--- airplay.py
+++ airplay.py
@@ -5,11 +5,9 @@ if __name__ == '__main__':
     for i in range(0, 22):
         for j in range(0, 22):
             if i == j:
-                text = "{} partout".format(i)
+                text = "{} all".format(i)
             else:
-                text = "{} à {}".format(i, j)
+                text = "{} to {}".format(i, j)
 
-            tts = gTTS(text=text, lang='fr')
-            tts.save("/home/thomas/sounds/{}_{}.mp3".format(i, j))
-
-
+            tts = gTTS(text=text, lang='en')
+            tts.save("/home/thomas/airplay/sounds_en/{}_{}.mp3".format(i, j))
